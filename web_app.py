329b793e820ed6caa85680cd39daeac3ddfd062b
Bishal Sarmah <99bishal7@gmail.com>
2024-05-22 20:01:17 +0530

    updated with web app

--- web_app.py
+++ web_app.py
@@ -16,29 +16,35 @@ def get_brand_name(car_name):
 
 cars_data['name'] = cars_data['name'].apply(get_brand_name)
 
-st.selectbox('Select Car Brand',cars_data['name'].unique())
+name = st.selectbox('Select Car Brand',cars_data['name'].unique())
 
-st.slider("Car Manufactured Year",1994,2020)
+year = st.slider("Car Manufactured Year",1994,2020)
 
-st.slider("No. of kilometers driven",10,500000)
+km_driven = st.slider("No. of kilometers driven",10,500000)
 
-st.selectbox('Select Fuel Type ',cars_data['fuel'].unique())
+fuel = st.selectbox('Select Fuel Type ',cars_data['fuel'].unique())
 
-st.selectbox('Seller Type',cars_data['seller_type'].unique())
+seller_type = st.selectbox('Seller Type',cars_data['seller_type'].unique())
 
-st.selectbox('Transmission Type',cars_data['transmission'].unique())
+transmission = st.selectbox('Transmission Type',cars_data['transmission'].unique())
 
-st.slider("Car Mileage kmpl ",1,40)
+owner = st.selectbox("Owner Type",cars_data['owner'].unique())
 
-st.slider("Car Engine in CC ",500,5000)
+mileage = st.slider("Car Mileage kmpl ",1,40)
 
-st.slider("Max Power of Car in bhp",0,250)
+engine = st.slider("Car Engine in CC ",500,5000)
 
-st.slider("Enter No. of seats",2,20)
+max_power = st.slider("Max Power of Car in bhp",0,250)
+
+seats = st.slider("Enter No. of seats",2,20)
 
 
 
 if st.button("Predict"):
-     
-
-
+    input_data_model = pd.DataFrame(
+        [[name, year, km_driven, fuel, seller_type, transmission, owner, mileage, engine, max_power, seats]],
+        columns=['name', 'year', 'km_driven', 'fuel', 'seller_type', 'transmission', 'owner', 'mileage', 'engine', 'max_power', 'seats']
+    )
+    st.write(input_data_model)
+    predict = pipeline.predict(input_data_model)
+    st.write(f"Predicted Car Price: {predict[0]}")
